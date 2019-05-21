a0011df508bd596ec9b67a78611bfdb0a0d487db
Džemil Džigal <37112548+ddzigal1@users.noreply.github.com>
2019-05-21 14:47:58 +0200

    Update koda za temperaturni senzor
    Kek

--- TemperaturniSenzorProba/TemperaturniSenzorProba.ino
+++ TemperaturniSenzorProba/TemperaturniSenzorProba.ino
@@ -12,14 +12,12 @@ DallasTemperature senzor(&oneWire);
 void setup(){
  //izlaz će biti na COM serijskom monitoru
   Serial.begin(9600);
-  Serial.println("Pokazni primjer temperaturnog senzora");
   senzor.begin(); 
+  
 }
 void loop(){
   // poziv requestTemperatures() traži vrijednosti temperatura svih povezanih senzora, u ovom slučaju samo jedan
-  Serial.println("Daj temperature...");
   senzor.requestTemperatures();
-  Serial.println("Dobio sam temperature.");
   Serial.println(senzor.getTempCByIndex(0));
   delay(1000);
 }
