2ca708edb2773616e259957ad24e71e5bab2abb6
Amila Borancic <aborancic1@etf.unsa.ba>
2019-05-25 18:14:34 +0200

    Ui za login KONACNO gotov

--- PSDS Mobilna/PSDS_Mobilna/Amila/Login.js
+++ PSDS Mobilna/PSDS_Mobilna/Amila/Login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button} from 'react-native';
 import Logo from "../assets/jemoLogo.png"
 import InputTexts from "./InputTexts";
 import Separator from "./Separator";
@@ -13,7 +13,7 @@ class Login extends React.Component {
 
     render() {
         return (
-            <View style={{ backgroundColor: "red", padding: 10 }}>
+            <View style={{padding: 10 }}>
                 <View>
                     <Image source={Logo} alt="help" style={{ width: 150, height: 150 }} />
                 </View>
@@ -21,6 +21,20 @@ class Login extends React.Component {
                 <InputTexts label="Email" placeholder="something@something.com" secure={false} stilTekst={style.text}/>
                 <Separator />
                 <InputTexts label="Password" placeholder="" secure={true}/>
+                <Separator />
+                <View style={{
+                    flexDirection:"row",
+                    flex:1
+                }}>
+                    <Text style={{
+                        textDecorationLine: 'underline',
+                        color: "#2d89ef",
+                        flex:2
+                    }}>Create Account</Text>
+                    <Button title="Sign in" style={{
+                        flex:2
+                    }}/>
+                </View>
             </View>
         );
     }
